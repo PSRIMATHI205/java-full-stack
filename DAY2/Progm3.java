0f9104fd27ec9e16856133d824b53edd12ba3f65
Srimathi <srimathiparthiban761@gmail.com>
2025-12-11 12:02:35 +0530

    day2 probs

---
+++ DAY2/Progm3.java
@@ -0,0 +1,15 @@
+public class Progm3 {
+    public static void main(String[] args) {
+        int n=1234;
+        int count =0;
+        // for(int i=n;i>0;i=i/10){
+        //     count++;
+        // }
+        while(n!=0){
+            n=n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+    
+}
---
+++ DAY2/Progm4.java
@@ -0,0 +1,7 @@
+public class Progm4 {
+    public static void main(String[] args) {
+        int n=j;
+        for(int i=n;)
+    }
+    
+}
