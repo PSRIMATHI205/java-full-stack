306a326ebbf48214f7c460682b7d2f83457927c2
Srimathi <srimathiparthiban761@gmail.com>
2025-12-12 13:51:55 +0530

    response noted

--- Day3/Prog9.java
+++ Day3/Prog9.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;//power of a number
 public class Prog9 {
     public static void main(String[] args) {
+        double base =2;
+        int power =-2;
+        double box
+
         
     }
     
