dbdb1ba4d5c437471792c718fcc228d099078e8c
Srimathi <srimathiparthiban761@gmail.com>
2025-12-19 14:53:23 +0530

    update

--- ECLIPSE/Encapsulation/src/Tamilnadu/delhi/B_branchOffice.java
+++ ECLIPSE/Encapsulation/src/Tamilnadu/delhi/B_branchOffice.java
@@ -1,5 +1,12 @@
 package Tamilnadu.delhi;
-
+import Tamilnadu.chennai.Office;
 public class B_branchOffice {
-
+	public static void main(String[] args) {
+	B_branchOffice off=new B_branchOffice();
+	off.working_time();
+	Office off1=new Office();
+	}
+	void working_time() {
+		System.out.println("working time 12 hours");
+	}
 }
